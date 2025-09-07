dcd86b02341c396a2bb8d046dd0d4b62526138c6
dev-ssj <ssj.sun.98@gmail.com>
2025-09-07 21:26:19 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.09 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -1,9 +1,7 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        for(int i=my_string.length() - 1; i>=0; i--){
-            answer = answer + my_string.charAt(i);
-        }
+        StringBuffer sb = new StringBuffer(my_string);
+        String answer = sb.reverse().toString();
         return answer;
     }
 }
