0e8383783fb8c7565ec97452056e9050a5ab35aa
dev-ssj <ssj.sun.98@gmail.com>
2025-09-08 18:46:39 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+
+        int sum = 0;
+        for(int i: numbers){
+            sum+=i;
+        }
+        answer = sum/(double)numbers.length;
+        return answer;
+    }
+}
