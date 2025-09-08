148d120a554be1732143a9a20fc6998ed76541dd
dev-ssj <ssj.sun.98@gmail.com>
2025-09-08 19:56:09 +0900

    [level 0] Title: 약수 구하기, Time: 0.41 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -6,17 +6,19 @@ class Solution {
         for(int i = 1; i <= n; i ++){
             if(n%i == 0) num ++;
         }
+        
+        //2. 구한 약수의 갯수로 배열 생성
         int[] answer = new int[num];
         
-        int k = 0;
+        int k = 0;  //배열 인덱스용 int 변수
+        
+        //3. 약수를 배열에 집어넣음(자동으로 오름차순으로 담아진다-> sort필요없음)
         for(int i = 1; i<= n; i++){
             if(n%i == 0){
                 answer[k] = i;
                 k++;
             }
         }
-        
-        Arrays.sort(answer);
         
         return answer;
     }
